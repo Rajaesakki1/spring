b6d18f90625a2bc53b4158a198c1279804c85b9f
Rajaesakki1 <rajaesakki1806@gmail.com>
2025-07-24 21:18:46 +0530

    pathvariable and requestparam

--- src/main/java/spring/io/HelloWorld/HelloWorldController.java
+++ src/main/java/spring/io/HelloWorld/HelloWorldController.java
@@ -1,6 +1,8 @@
 package spring.io.HelloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,9 @@ public class HelloWorldController {
     @GetMapping("/helo2")
     String getindentity(){
         return "page2";
+    }
+    @GetMapping("/hi")
+    String getting(@RequestParam (defaultValue = "raji")String name){
+        return "Hello"+name;
     }
 }
